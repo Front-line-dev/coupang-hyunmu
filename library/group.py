c0bffcfe733b5417676c5463f11fd2229b015ea3
Front-line-dev <kimminsung@kakao.com>
2021-07-14 22:25:36 +0900

    feat: change how to count

--- library/group.py
+++ library/group.py
@@ -1,8 +1,9 @@
-product_set = set()
-
 def compare_histories(history_list):
+    product_set = set()
+    total_products = 0
+
     for history in history_list:
         product_set.update(tuple(history))
+        total_products += len(history)
     
-    total_products = len(history_list) * len(history_list[0])
     return (total_products - len(product_set)) / total_products
